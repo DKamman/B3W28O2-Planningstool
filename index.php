a0289b53fb5bc0d6b374ffc60e5c3303d68fca76
Unknown <99053978@mydavinci.nl>
2019-04-13 16:54:58 +0200

    commit

---
+++ index.php
@@ -0,0 +1,78 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "mysql";
+$myDB = "planningtool";
+
+header('Content-type: text/html; charset=iso-8859-1');
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$myDB", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+}
+catch(PDOException $e)
+{
+  echo "Connection failed: " . $e->getMessage();
+}
+
+$sql = 'SELECT * FROM games';
+$query = $conn->prepare($sql);
+$query->execute();
+
+$result = $query->fetchAll();
+// var_dump($result);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+    <div class="card" style="width: 18rem;">
+      <?php
+      foreach ($result as $row) {
+      ?>
+      <img src="img/<?php echo $row['image']?>" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title"><?php echo $row['name']?></h5>
+        <p class="card-text"><?php echo $row['description']?></p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+      <?php
+      }
+      ?>
+    </div>
+
+    <table border="1">
+      <?php
+      foreach ($result as $row) {
+      ?>
+      <tr>
+        <td><?php echo $row['id']?></td>
+        <td><?php echo $row['name']?></td>
+        <td><img src="img/<?php echo $row['image']?>"></td>
+        <td><?php echo $row['description']?></td>
+        <td><?php echo $row['expansions']?></td>
+        <td><?php echo $row['skills']?></td>
+      </tr>
+      <?php
+      }
+      ?>
+    </table>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
